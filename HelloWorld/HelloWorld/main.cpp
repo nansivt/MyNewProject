371129220e3f3f11ee9f84d53263b5408bb72cfe
Nan Si <nansi@Nans-MacBook-Pro.local>
2019-06-24 22:52:18 -0400

    Establishing Establish connection btw. local and remote repository. Upload some files and directories.

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Nan Si on 11/12/17.
+//  Copyright © 2017 Nan Si. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    printf("Hello, World! \n");
+    return 0;
+}
